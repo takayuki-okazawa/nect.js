b0e6387cb6cf31695c435d52ecac47307d4dbb83
Takayuki Okazawa <takayuki.hack@gmail.com>
2013-07-25 07:53:33 +0900

    Add file

---
+++ nect.js
@@ -0,0 +1,22 @@
+/* nect.js */
+
+/* Propaty */
+var allElements;
+
+/* Init */
+var init = function(){
+	//Add Submit Action to Button
+
+	//Get All Elements
+	//get id
+	//insert Array
+}
+
+/* Send json */
+var to = function(url){
+	//for elements array
+		//create json
+	
+	//send json
+	return response;
+}
